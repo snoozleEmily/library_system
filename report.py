8e9a968969238f909fab69f1944e3c7950a690ce
Emily Alvim <105649387+snoozleEmily@users.noreply.github.com>
2024-05-28 22:32:45 -0300

    Fixed book_stock Data Type Error
    - Fixed broken logic in the book module. The is_available variable was always returning True; now it correctly returns True only if a copy is available. This issue was causing a problem in the report module where book_stock was sometimes an integer and other times a list.
    - Fixed the 'Copias Em Estoque' display. It now shows the number of items (integer) in the book_stock list.
    - Removed unnecessary code throughout the system.

--- report.py
+++ report.py
@@ -1,37 +1,64 @@
 from dash import *
 from fetch_data import *
 
-# Pega os dados do json
-users = data.get('users', [])
-books = data.get('books', [])
-
-# 
+# Comentário bom
 individual_book = ''
 all_books = []
+
 individual_user = ''
 all_users = []
 
+def all_books_report():
+    for book in data['books']:
+        book_title = book['Título']
+        book_author = book['Autor']
+        book_year = book['Ano de Publicação']
+        book_stock = book['Copias em Estoque']
+        book_available = book['Copias Disponíveis']
+        
+        # Conta o número de cópias em estoque
+        copies_stock = 0
+        for _ in book_stock: 
+          copies_stock += 1   
 
-for book in data['books']:
-    book_id = book.get('ID', '')
-    book_title = book.get('Título', '')
-    book_author = book.get('Autor', '')
-    book_year = book.get('Ano de Publicação', '')
-    book_stock = book.get('Copias em Estoque', '')
-    book_available = book.get('Copias Disponíveis', '')
+        individual_book = f'//Livro: {book_title} | Autor(a): {book_author} | Ano De Publicação: {book_year} | Copias Em Estoque: {copies_stock} | Copias Disponíveis Para Empréstimo: {book_available} '
+        all_books.append(individual_book)
 
-    individual_book = f'//Livro: {book_title} - Autor(a): {book_author} - Ano de Publicação: {book_year} - Copias em Estoque: {book_stock} - Copias Disponíveis: {book_available}. '
-    all_books.append(individual_book)
+        # Lista de todos os livros    
+        for individual_book in all_books:
+            dash()
+            print(individual_book)
+            dash()
 
+def all_users_report():
+    for user in data['users']:
+        user_name = user['Nome']
+        user_id = user['CPF']
+        user_info = user['Contato']
+        user_loan = user['Livro Em Posse']        
 
-# Lista de todos os livros    
-for individual_book in all_books:
-    space()
-    print(individual_book)
-    dash()
+        individual_user = f'//Nome: {user_name} | CPF: {user_id} | Contato: {user_info} | Livro Em Posse: {user_loan} '
+        all_users.append(individual_user)
+
+        # Lista de todos os livros    
+        for individual_user in all_users:
+            dash()
+            print(individual_user)
+            dash()
 
 #Livros disponíveis
-#Livros cadastrados
+#def available_books_report():
+#    pass
 
-#Usuários cadastrados
+#Livros cadastrados
+#def stock_books_report():
+#    pass
+
 #Relatório Geral
+def generate_general_report():
+    all_books_report()
+    all_users_report()
+    # Os valores se repetem?
+
+
+generate_general_report()
