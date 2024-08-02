d1713b28dff1918868f563b9c774024745249f88
snoozleEmily <emilyalvimicm@gmail.com>
2024-08-02 16:37:14 -0300

    Update dash.py

--- dash.py
+++ dash.py
@@ -1,10 +1,10 @@
-def dash():
+def dash() -> None:
     '''
     Imprime uma linha pontilhada para melhorar a legibilidade no console
     '''
     print('----' * 10)
 
-def space():
+def space() -> None:
     '''
     Imprime um espaço de uma linha para melhorar a organização do conteúdo
     '''
